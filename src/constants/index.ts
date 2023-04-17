15513f8e9fe665d35c1211a25a7087cf54c3442f
warewar1 <110954486+warewar1@users.noreply.github.com>
2023-04-17 19:08:45 +0800

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,8 +14,7 @@ export const BASES_TO_CHECK_TRADES_AGAINST: { readonly [chainId in ChainId]: Tok
   [ChainId.ROPSTEN]: [WETH[ChainId.ROPSTEN]],
   [ChainId.RINKEBY]: [WETH[ChainId.RINKEBY]],
   [ChainId.GÖRLI]: [WETH[ChainId.GÖRLI]],
-  [ChainId.KOVAN]: [WETH[ChainId.KOVAN]],
-  [ChainId.SIRIUSNET]: [WETH[ChainId.SIRIUSNET]]
+  [ChainId.KOVAN]: [WETH[ChainId.KOVAN]]
 }
 
 // used for display in the default list when adding liquidity
